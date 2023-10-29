214f61991221df01bc3e3c5b8946ecef4115d8b8
Filiph Siitam Sandström <filiph@nordcom.io>
2023-10-29 18:25:26 +0100

    CI: Add `husky` and `lint-staged`.

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,25 @@
+import { ESLint } from 'eslint';
+import { relative } from 'node:path';
+
+const filterIgnoredFiles = async (files) => {
+    const cwd = process.cwd();
+    const eslint = new ESLint();
+    const relativePaths = files.map((file) => relative(cwd, file));
+    const isIgnored = await Promise.all(relativePaths.map((file) => eslint.isPathIgnored(file)));
+    const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+
+    return filteredFiles.join(' ');
+};
+
+export default {
+    '**/*.{js,ts,jsx,tsx}': async (files) => {
+        const filesToLint = await filterIgnoredFiles(files);
+
+        return [`eslint -c .eslintrc.json --max-warnings=0 --fix ${filesToLint}`];
+    },
+    '**/*.css': async (files) => {
+        const filesToLint = await filterIgnoredFiles(files);
+
+        return [`prettier --ignore-path --write ${filesToLint}`];
+    },
+};
