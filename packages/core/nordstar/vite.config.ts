4f23ed7c0ad6ac208066370b736273046c1af4fd
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-17 07:39:28 +0000

    Build: Migrate to `vite` (resolves STAR-6).

---
+++ packages/core/nordstar/vite.config.ts
@@ -0,0 +1,31 @@
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        target: 'esnext',
+        copyPublicDir: false,
+        emptyOutDir: true,
+        outDir: resolve(__dirname, 'dist'),
+        sourcemap: true,
+        lib: {
+            entry: ['index.ts'],
+            formats: ['es']
+        },
+        rollupOptions: {}
+    },
+    plugins: [
+        react(),
+        tsConfigPaths(),
+        dts({
+            clearPureImport: false,
+            rollupTypes: false,
+            insertTypesEntry: true,
+            tsconfigPath: resolve(__dirname, 'tsconfig.json')
+        })
+    ]
+});
