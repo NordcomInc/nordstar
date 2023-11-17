54e7ba4b91f90c91bb3faabbdfc51add5cc158c1
Filiph Siitam Sandström <filiph@nordcom.io>
2023-11-17 13:01:01 +0000

    Components: Add global styling.

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Card, Heading, View } from '@nordcom/nordstar';
+import type { Metadata } from 'next';
 import styles from './page.module.scss';
+
+export const metadata: Metadata = {
+    title: 'Nordstar',
+    alternates: {
+        canonical: 'https://nordstar.nordcom.io/'
+    }
+};
 
 export default async function IndexPage() {
     return (
         <View className={styles.container}>
-            <div className={styles.title}>
+            <div className={styles.header}>
                 <Heading>Nordstar</Heading>
                 <Heading level="h2">
                     An opinionated component library for building human-centric user interfaces
@@ -12,14 +20,38 @@ export default async function IndexPage() {
             </div>
 
             <div className={styles.content}>
-                <Card>
-                    <Heading level="h3">Guides</Heading>
+                <Card className={styles.block}>
+                    <Heading level="h4" as="p">
+                        Guides
+                    </Heading>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Vitae elementum curabitur vitae nunc. Turpis egestas maecenas
+                        pharetra convallis posuere morbi leo urna. Amet consectetur adipiscing elit ut aliquam purus.
+                        Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo.
+                    </p>
                 </Card>
-                <Card>
-                    <Heading level="h3">Examples</Heading>
+                <Card className={styles.block}>
+                    <Heading level="h4" as="p">
+                        Examples
+                    </Heading>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Vitae elementum curabitur vitae nunc. Turpis egestas maecenas
+                        pharetra convallis posuere morbi leo urna. Amet consectetur adipiscing elit ut aliquam purus.
+                        Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo.
+                    </p>
                 </Card>
-                <Card>
-                    <Heading level="h3">Documentation</Heading>
+                <Card className={styles.block}>
+                    <Heading level="h4" as="p">
+                        Documentation
+                    </Heading>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Vitae elementum curabitur vitae nunc. Turpis egestas maecenas
+                        pharetra convallis posuere morbi leo urna. Amet consectetur adipiscing elit ut aliquam purus.
+                        Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo.
+                    </p>
                 </Card>
             </div>
         </View>
