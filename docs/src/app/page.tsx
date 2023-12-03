020e2dfcdc5c7b2310e47839d2fe0537cd864904
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-03 07:50:10 +0100

    Docs: Use the new Accented component.

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,9 +1,13 @@
-import { Card, Heading, View } from '@nordcom/nordstar';
+import { Accented, Card, Heading, View } from '@nordcom/nordstar';
 import type { Metadata } from 'next';
 import styles from './page.module.scss';
 
 export const metadata: Metadata = {
-    title: 'Nordstar',
+    title: {
+        absolute: 'Nordstar Component Library'
+    },
+    description:
+        'An opinionated component library for building human-centric user interfaces by Nordcom Group Inc. and contributors.',
     alternates: {
         canonical: 'https://nordstar.nordcom.io/'
     }
@@ -13,9 +17,11 @@ export default async function IndexPage() {
     return (
         <View className={styles.container}>
             <div className={styles.header}>
-                <Heading>Nordstar</Heading>
+                <Heading>
+                    Nordstar by <Accented>Nordcom</Accented> Group Inc. and contributors
+                </Heading>
                 <Heading level="h2">
-                    An opinionated component library for building human-centric user interfaces
+                    An opinionated component library for building <Accented>human</Accented>-centric user interfaces
                 </Heading>
             </div>
 
