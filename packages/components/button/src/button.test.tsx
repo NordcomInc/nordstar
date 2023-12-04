6bb5f73b83d9aa408417246cce5cb2ae15706f62
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-04 04:49:17 +0100

    Button: Add missing test-cases.

--- packages/components/button/src/button.test.tsx
+++ packages/components/button/src/button.test.tsx
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest';
 
-import { Button } from '../src';
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
+import { Button } from '../src';
 
 describe('components', () => {
     describe('button', () => {
@@ -10,6 +10,28 @@ describe('components', () => {
             it('should render correctly', () => {
                 const wrapper = render(<Button />);
 
+                expect(() => wrapper.unmount()).not.toThrow();
+            });
+
+            it('should render with disabled correctly', () => {
+                const wrapper = render(<Button disabled />);
+
+                expect(wrapper.getByRole('button')).toHaveProperty('disabled', true);
+                expect(wrapper.getByRole('button')).toHaveAttribute('aria-disabled', 'true');
+                expect(() => wrapper.unmount()).not.toThrow();
+            });
+
+            it('renders as the selected variant', () => {
+                const wrapper = render(<Button variant="outline" />);
+
+                expect(wrapper.getByRole('button')).toHaveAttribute('data-variant', 'outline');
+                expect(() => wrapper.unmount()).not.toThrow();
+            });
+
+            it('renders as the selected color', () => {
+                const wrapper = render(<Button color="primary" />);
+
+                expect(wrapper.getByRole('button')).toHaveAttribute('data-color', 'primary');
                 expect(() => wrapper.unmount()).not.toThrow();
             });
         });
