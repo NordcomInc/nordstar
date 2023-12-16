aa2caea986946cb7b8fa462ba50dbb16ca821c75
Filiph Siitam Sandström <filiph@nordcom.io>
2023-12-16 23:09:48 +0100

    Tests: Remove left-overs.

--- .vitest/setup.ts
+++ .vitest/setup.ts
@@ -1,17 +1,11 @@
 import '@testing-library/jest-dom/vitest';
-import 'next';
 
 import * as matchers from '@testing-library/jest-dom/matchers';
 
 import { cleanup } from '@testing-library/react';
-import { afterEach, expect, vi } from 'vitest';
+import { afterEach, expect } from 'vitest';
 
 expect.extend(matchers);
 afterEach(() => {
     cleanup();
-});
-
-// Mock the `server-only` module as it doesn't work with vitest.
-vi.mock('server-only', () => {
-    return {};
 });
