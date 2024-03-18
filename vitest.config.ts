547b5f11756096207ef3b0002ed1bbca0423ba2b
Filiph Siitam Sandström <filiph@nordcom.io>
2024-03-18 11:12:52 +0100

    Meta: Slightly improve vitest setup.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,52 +1,74 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { defineConfig } from 'vitest/config';
 
 import base from './vite.config';
 
 const reporters = ['verbose'];
 const extraReporters = !!process.env.GITHUB_ACTIONS ? ['github-actions'] : [];
 
-export default mergeConfig(
-    base,
-    defineConfig({
-        test: {
-            bail: 2,
-            environment: 'happy-dom',
-            exclude: ['**/*.d.ts', '**/*.stories.*', '**/dist/**/*.*', '**/node_modules/**/*.*'],
-            globals: true,
-            maxConcurrency: 16,
-            passWithNoTests: true,
-            reporters: [...reporters, ...extraReporters],
-            setupFiles: ['./.vitest/setup.ts'],
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-            browser: {
-                name: 'edge',
-                provider: 'playwright',
-                providerOptions: {
-                    enabled: false
-                }
+export default defineConfig({
+    root: resolve(__dirname),
+    optimizeDeps: {
+        force: true,
+        esbuildOptions: {
+            define: {
+                global: 'globalThis'
             },
-
-            coverage: {
-                all: true,
-                exclude: [
-                    '**/*.d.ts',
-                    '**/*.test.*',
-                    '**/*.stories.*',
-                    '**/src/app/{page,layout}.*',
-                    '**/packages/**/src/index.*',
-                    '**/*.css',
-                    '**/dist/**/*.*',
-                    'packages/core/system/src/colors.ts'
-                ],
-                include: ['**/src/**/*.{js,ts,jsx,tsx}'],
-                provider: 'v8',
-                reporter: ['text', 'json'],
-                reportOnFailure: true
-            },
-
-            typecheck: {
-                tsconfig: './tsconfig.test.json'
-            }
+            plugins: []
         }
-    })
-);
+    },
+    test: {
+        bail: 2,
+        environment: 'happy-dom',
+        exclude: ['**/*.d.ts', '**/*.stories.*', '**/dist/**/*.*', '**/node_modules/**/*.*'],
+        globals: true,
+        maxConcurrency: 16,
+        passWithNoTests: true,
+        silent: false,
+        reporters: [...reporters, ...extraReporters],
+
+        pool: 'vmThreads',
+        poolOptions: {
+            vmThreads: {
+                useAtomics: true
+            }
+        },
+
+        setupFiles: ['./.vitest/setup.ts'],
+
+        browser: {
+            name: 'edge',
+            provider: 'playwright',
+            providerOptions: {
+                enabled: false
+            }
+        },
+
+        coverage: {
+            all: true,
+            exclude: [
+                '**/*.css',
+                '**/*.d.ts',
+                '**/*.stories.*',
+                '**/*.test.*',
+                '**/dist/**/*.*',
+                '**/node_modules/**/*.*',
+                '**/packages/**/src/index.*',
+                '**/src/app/{page,layout}.*',
+                'packages/core/system/src/colors.ts'
+            ],
+            include: ['**/src/**/*.{js,ts,jsx,tsx}'],
+            provider: 'v8',
+            reporter: ['json-summary'],
+            reportOnFailure: true
+        },
+
+        typecheck: {
+            tsconfig: './tsconfig.test.json'
+        }
+    }
+});
