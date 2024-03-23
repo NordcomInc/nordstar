61c20e66c503477a317c40e22368e5d8ca20e3bb
Filiph Siitam Sandström <filiph@nordcom.io>
2024-03-23 14:20:47 +0000

    Input: Fix `'use client';` duplication.

--- packages/components/input/vite.config.ts
+++ packages/components/input/vite.config.ts
@@ -1,4 +1,3 @@
-import MagicString from 'magic-string';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -22,22 +21,6 @@ export default mergeConfig(
             }
         },
         plugins: [
-            {
-                // TODO: Turn this into a proper plugin or figure out a better way to do this.
-                name: 'nordcom:use-client',
-                apply: 'build',
-                enforce: 'post',
-                renderChunk(code, chunk) {
-                    const ms = new MagicString(code);
-
-                    if (chunk.viteMetadata) ms.prepend(`'use client';\n\n`);
-
-                    return {
-                        code: ms.toString(),
-                        map: ms.generateMap()
-                    };
-                }
-            },
             codecovVitePlugin({
                 enableBundleAnalysis: !!process.env.CODECOV_TOKEN,
                 bundleName: name,
