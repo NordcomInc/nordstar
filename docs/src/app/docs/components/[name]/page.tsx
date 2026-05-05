28401b0f7ce6f3f5fe173dd2270047dfef013b76
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-05 22:43:58 +0200

    chore: Fix params usage

--- docs/src/app/docs/components/[name]/page.tsx
+++ docs/src/app/docs/components/[name]/page.tsx
@@ -6,7 +6,9 @@ export type ComponentDocsPageParams = {
     name: string;
 };
 
-export async function generateMetadata({ params: { name } }: { params: ComponentDocsPageParams }): Promise<Metadata> {
+export async function generateMetadata({ params }: { params: ComponentDocsPageParams }): Promise<Metadata> {
+    const { name } = await params;
+
     const { key } = getComponentByName(name);
 
     return {
@@ -21,7 +23,9 @@ export function generateStaticParams(): ComponentDocsPageParams[] {
     return getAvailableComponents().map((name) => ({ name: name.toLowerCase() }));
 }
 
-export default async function DocsComponentsComponentPage({ params: { name } }: { params: ComponentDocsPageParams }) {
+export default async function DocsComponentsComponentPage({ params }: { params: ComponentDocsPageParams }) {
+    const { name } = await params;
+
     const { Component, key } = getComponentByName(name);
     const SpoofedComponent = Component as React.ComponentType<any>;
 
