b6b7ba0a495997fcea200fc51d2be65eb0a9a9b1
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-05 20:22:09 +0200

    build(storybook): switch to @tailwindcss/postcss, bump tailwind to v4

--- packages/storybook/postcss.config.cjs
+++ packages/storybook/postcss.config.cjs
@@ -1,7 +1,4 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-const path = require('node:path');
-const process = require('node:process');
-
 module.exports = {
-    plugins: [require('postcss-import')(), require('tailwindcss')('tailwind.config.cjs'), require('autoprefixer')]
+    plugins: [require('@tailwindcss/postcss')]
 };
