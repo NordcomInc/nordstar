acd41caf28d9709d73eb5d40475c82fb7c02dc18
Filiph Siitam Sandström <filfat@hotmail.se>
2026-05-05 23:09:13 +0200

    chore: remove ESLint, Prettier, and lint-staged configs

--- .prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-const prettierConfig = require('@nordcom/prettier');
-
-/** @type {import('prettier').Config} */
-module.exports = {
-    ...prettierConfig,
-    $schema: 'http://json.schemastore.org/prettierrc',
-    plugins: [...(prettierConfig.plugins || []), 'prettier-plugin-tailwindcss'],
-    quoteProps: 'consistent'
-};
